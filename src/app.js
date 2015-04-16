be00ab94ac8e57d5655917da8655b4b09727b7e8
Kyle Reden <kylereden@gmail.com>
2015-04-16 00:24:48 -0500

    going through tutorial

--- src/app.js
+++ src/app.js
@@ -5,54 +5,41 @@
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
+var ajax = require('ajax');
+// Create a Card with title and subtitle
+var card = new UI.Card({
+  title:'Weather',
+  subtitle:'Fetching...'
+}),
+  cityName = 'Chicago';
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
-});
+ajax(
+  {
+  url: url,
+  type: 'json'
+  },
+  function(data) {
+    console.log('Data retrieved');
+    
+    // Extract data
+    var location = data.name;
+//     var temperature = Math.round(data.main.temp - 273.15) + 'C';
+    var temp = Math.round( (data.main.temp * 9/5) - 459.67) + 'F';
+//     [K] × 9⁄5 − 459.67
 
-main.show();
+    // Always upper-case first letter of description
+    var description = data.weather[0].description;
+    description = description.charAt(0).toUpperCase() + description.substring(1);
+    
+    // Show to user
+    card.subtitle(location + ', ' + temp);
+    card.body(description);
+  },
+  function(error) {
+    console.log('Something bad happened... ', error);
+  }
+);
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+// Display the Card
+card.show();
