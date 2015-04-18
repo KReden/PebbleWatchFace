6de765d236b80d8fb3d5ea00671a4bd6fef4a5bf
Kyle Reden <kreden@pathf.com>
2015-04-18 18:46:51 -0500

    remove some comments

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@
 
 var UI = require('ui');
 var ajax = require('ajax');
-// Create a Card with title and subtitle
 var card = new UI.Card({
   title:'Weather',
   subtitle:'Fetching...'
@@ -20,18 +19,13 @@ ajax(
   type: 'json'
   },
   function(data) {
-    console.log('Data retrieved');
-    
-    // Extract data
     var location = data.name;
-//     var temperature = Math.round(data.main.temp - 273.15) + 'C';
     var temp = Math.round( (data.main.temp * 9/5) - 459.67) + 'F';
-//     [K] × 9⁄5 − 459.67
 
     // Always upper-case first letter of description
     var description = data.weather[0].description;
     description = description.charAt(0).toUpperCase() + description.substring(1);
-    
+
     // Show to user
     card.subtitle(location + ', ' + temp);
     card.body(description);
